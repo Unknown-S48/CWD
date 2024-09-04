1c95fe4ffddadfaf433becf38089a0e23aa71d21
Wian <wian.sandenbergh.2005@gmail.com>
2024-09-04 13:47:57 +0200

    Update to Login

--- JS/Login.js
+++ JS/Login.js
@@ -6,12 +6,4 @@ function Login() {
     var User_Name = document.getElementById("user_name").value;
     localStorage.setItem("User_Name", User_Name);
     console.log(User_Name);
-    
-    //location.replace("youtube.com");
-    //window.location.assign('home.html');
-    //document.close("index.html");
-    open("home.html");
 }
-
-if( may_continue )
-setTimeout(home(), 2000);
